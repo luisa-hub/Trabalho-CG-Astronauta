2b97020657a695f1b8963a63eab04b0a21b184e8
Greygor <breno.rocha.maia@gmail.com>
2022-02-23 23:36:36 -0300

    mais updates

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var cena = new THREE.Scene(); //Cria cena
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000); //Cria camera
+//var camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000); //Cria camera
 
 var render = new THREE.WebGLRenderer(); //cria render
 render.setSize(window.innerWidth, window.innerHeight);
@@ -16,12 +16,14 @@ cena.add(luzAmbiente2);
 
 
 
-var materialChao = new THREE.MeshPhongMaterial({color: 0x454f7a});
+//var materialChao = new THREE.MeshPhongMaterial({color: 0x454f7a});
+var materialChao = new THREE.TextureLoader().load('js/lua.jpg');
 var geoChao = new THREE.PlaneGeometry(5000, 5000, 100, 100);
 var chao = new THREE.Mesh(geoChao, materialChao);
 chao.position.x = 0
 chao.position.y = 0
 chao.position.z = 0
+chao.rotation.x = Math.PI/2 + Math.PI
 cena.add(chao)
 
 var geometriaOcto = new THREE.OctahedronGeometry(1,1,1);
@@ -30,42 +32,44 @@ var Octo = new THREE.Mesh(geometriaOcto, materialOCto);
 cena.add(Octo)
 
 
-camera.position.x = 2
-camera.position.y = 6
-camera.position.z = 2
+//camera.position.x = 2
+//camera.position.y = 6
+//camera.position.z = 2
 
 var x = 0;
 var xi;
 var yi;
 
-canvas.addEventListener("mousedown", function (e){
-    xi = e.offsetX;
-    yi = e.offsetY;
-}, false);
+//canvas.addEventListener("mousedown", function (e){
+//    xi = e.offsetX;
+//    yi = e.offsetY;
+//}, false);
 
-canvas.addEventListener("mousemove", function (e){
-    if (e.buttons > 0){
-        camera.position.x = 8 * (xi - e.offsetX)/canvas.width;
-        camera.position.y = 8 * (e.offsetY - yi)/canvas.height;
-        
+//canvas.addEventListener("mousemove", function (e){
+//    if (e.buttons > 0){
+//        camera.position.x = 8 * (xi - e.offsetX)/canvas.width;
+ //       camera.position.y = 8 * (e.offsetY - yi)/canvas.height;
+//        
+//
+ //   }
+//}
+//,false);
 
-    }
-}
-,false);
-
-var cubeGeometry = new THREE.BoxGeometry (10,10,10);
+var cubeGeometry = new THREE.BoxGeometry (3,3,3);
     var cubeMaterial = new THREE.MeshBasicMaterial ({color: 0x1ec876});
     cube = new THREE.Mesh (cubeGeometry, cubeMaterial);
 
-    cube.position.set (0, 0, 0);
+    cube.position.set (0, 3, 0);
     cena.add (cube);
 
-    camera = new THREE.PerspectiveCamera (45, window.innerWidth/window.innerHeight, 1, 10000);
-    camera.position.y = 460;
+    camera = new THREE.PerspectiveCamera (45, window.innerWidth/window.innerHeight, 1, 1000);
+    camera.position.y = 260;
     camera.position.z = 400;
-    camera.lookAt (new THREE.Vector3(0,0,0));
+    camera.enableZoom = true;
+    //camera.autoRotate = true;
+    //camera.lookAt (new THREE.Vector3(5,90,5));
 
-    controls = new THREE.OrbitControls (camera, canvas.domElement);
+    controls = new THREE.FirstPersonControls (camera, canvas.domElement);
     
     var gridXZ = new THREE.GridHelper(100, 10);
     gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
@@ -74,6 +78,8 @@ var cubeGeometry = new THREE.BoxGeometry (10,10,10);
 function desenhar() {
     render.render(cena, camera)
     requestAnimationFrame(desenhar);
+    controls.update();
+    movimentar()
 
 
 
@@ -84,3 +90,51 @@ function desenhar() {
 
 requestAnimationFrame(desenhar);
 
+var teclas = [];
+
+document.onkeydown = function (evt) {
+    teclas[evt.keyCode] = true;
+}
+
+document.onkeyup = function (evt) {
+    teclas[evt.keyCode] = false;
+}
+
+function movimentar() {
+    if (teclas[65]) {// A
+        //Tecla a
+        console.log('clicado')
+        camera.position.z += 1
+    }
+    if (teclas[68]) {// D
+        //Tecla d
+        camera.position.y += 1
+    }
+    if (teclas[87]) {// W
+        // Tecla w
+        camera.position.y += 80
+    }
+
+    if (teclas[83]) {// S
+        // Tecla s
+        camera.position.y += 80
+    }
+    
+    if(teclas[37]){// seta esquerda
+         // Tecla Seta Esquerda
+    }
+    if(teclas[39]){// seta direira
+         // Tecla Seta Direita
+    }
+
+    if(teclas[40]){// seta baixo
+        // Tecla Seta baixo
+   }
+   if(teclas[38]){// seta cima
+        // Tecla Seta cima
+   }
+
+   if(teclas[32]){// epasso
+    //espaço
+   }
+}
