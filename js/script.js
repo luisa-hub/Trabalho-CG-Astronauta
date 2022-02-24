986f5c3834f36da40d185e982946f59a89c1576f
Greygor <breno.rocha.maia@gmail.com>
2022-02-23 21:51:41 -0300

    mais codigo

--- js/script.js
+++ js/script.js
@@ -53,127 +53,34 @@ canvas.addEventListener("mousemove", function (e){
 }
 ,false);
 
-let raycaster
-raycaster = new THREE.Raycaster( new THREE.Vector3(), new THREE.Vector3( 0, - 1, 0 ), 0, 10 );
-controls = new PointerLockControls( camera, document.body );
+var cubeGeometry = new THREE.BoxGeometry (10,10,10);
+    var cubeMaterial = new THREE.MeshBasicMaterial ({color: 0x1ec876});
+    cube = new THREE.Mesh (cubeGeometry, cubeMaterial);
+
+    cube.position.set (0, 0, 0);
+    cena.add (cube);
+
+    camera = new THREE.PerspectiveCamera (45, window.innerWidth/window.innerHeight, 1, 10000);
+    camera.position.y = 460;
+    camera.position.z = 400;
+    camera.lookAt (new THREE.Vector3(0,0,0));
+
+    controls = new THREE.OrbitControls (camera, canvas.domElement);
+    
+    var gridXZ = new THREE.GridHelper(100, 10);
+    gridXZ.setColors( new THREE.Color(0xff0000), new THREE.Color(0xffffff) );
+    cena.add(gridXZ);
 
 function desenhar() {
     render.render(cena, camera)
     requestAnimationFrame(desenhar);
 
 
-    raycaster.ray.origin.copy( controls.getObject().position );
-					raycaster.ray.origin.y -= 10;
-
-					const intersections = raycaster.intersectObjects( objects, false );
-
-					const onObject = intersections.length > 0;
-
-					const delta = ( time - prevTime ) / 1000;
-
-					velocity.x -= velocity.x * 10.0 * delta;
-					velocity.z -= velocity.z * 10.0 * delta;
-
-					velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
-
-					direction.z = Number( moveForward ) - Number( moveBackward );
-					direction.x = Number( moveRight ) - Number( moveLeft );
-					direction.normalize(); // this ensures consistent movements in all directions
-
-					if ( moveForward || moveBackward ) velocity.z -= direction.z * 400.0 * delta;
-					if ( moveLeft || moveRight ) velocity.x -= direction.x * 400.0 * delta;
-
-					if ( onObject === true ) {
-
-						velocity.y = Math.max( 0, velocity.y );
-						canJump = true;
-
-					}
-
-					controls.moveRight( - velocity.x * delta );
-					controls.moveForward( - velocity.z * delta );
-
-					controls.getObject().position.y += ( velocity.y * delta ); // new behavior
-
-					if ( controls.getObject().position.y < 10 ) {
-
-						velocity.y = 0;
-						controls.getObject().position.y = 10;
-
-						canJump = true;
-
-					}
 
 				
 
-
 }
+
 
 requestAnimationFrame(desenhar);
 
-const onKeyDown = function ( event ) {
-
-    switch ( event.code ) {
-
-        case 'ArrowUp':
-        case 'KeyW':
-            moveForward = true;
-            break;
-
-        case 'ArrowLeft':
-        case 'KeyA':
-            moveLeft = true;
-            break;
-
-        case 'ArrowDown':
-        case 'KeyS':
-            moveBackward = true;
-            break;
-
-        case 'ArrowRight':
-        case 'KeyD':
-            moveRight = true;
-            break;
-
-        case 'Space':
-            if ( canJump === true ) velocity.y += 350;
-            canJump = false;
-            break;
-
-    }
-
-};
-
-const onKeyUp = function ( event ) {
-
-    switch ( event.code ) {
-
-        case 'ArrowUp':
-        case 'KeyW':
-            moveForward = false;
-            break;
-
-        case 'ArrowLeft':
-        case 'KeyA':
-            moveLeft = false;
-            break;
-
-        case 'ArrowDown':
-        case 'KeyS':
-            moveBackward = false;
-            break;
-
-        case 'ArrowRight':
-        case 'KeyD':
-            moveRight = false;
-            break;
-
-    }
-
-};
-
-let moveForward = false;
-let moveBackward = false;
-let moveLeft = false;
-let moveRight = false;
-let canJump = false;
